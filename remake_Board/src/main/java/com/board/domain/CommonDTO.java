3d8d2901e4d722edfaa0e7f4c043117758d67edc
SangHun <shzero211@naver.com>
2021-12-19 16:00:22 +0900

    comment final

--- remake_Board/src/main/java/com/board/domain/CommonDTO.java
+++ remake_Board/src/main/java/com/board/domain/CommonDTO.java
@@ -1,9 +1,13 @@
 package com.board.domain;
 
 import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.board.paging.Criteria;
 import com.board.paging.PaginationInfo;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,12 +23,15 @@ public class CommonDTO extends Criteria {
 	private String deleteYn;
 
 	/** 등록일 */
-	private LocalDateTime insertTime;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	private Date insertTime;
 
 	/** 수정일 */
-	private LocalDateTime updateTime;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	private Date updateTime;
 
 	/** 삭제일 */
-	private LocalDateTime deleteTime;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	private Date deleteTime;
 
 }
